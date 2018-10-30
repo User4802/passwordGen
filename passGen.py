ed95e3a03e9893e1f573d74c016463269ea5c8e8
user4802 <24318149+User4802@users.noreply.github.com>
2018-10-30 15:14:22 -0400

    Basic tuple lib

--- passGen.py
+++ passGen.py
@@ -1,18 +1,4 @@
-import random as rd
-
-lenght = int(input("lenght"))
-
 number = ("1","2","3","4","5","6","7","8","9","0")
 special = ("!","@","#","$","%","?","&","*","(",")","-","_","=","+","/",".",","," ")
 alpha = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-alphaCap = ("A","B","C","D","E","F","G","H","I","J","K","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-
-
-
-print(alpha)
-print(alphaCap)
-
-#for i in range(lenght):
-    #passW = ""
-    #print(passW + number[rd.randrange(0,len(number))])
-#print(special[rd.randrange(0,len(special))] + special[rd.randrange(0,len(special))])
+alphaCap = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
